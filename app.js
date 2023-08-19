d89cf34517eeba350794b71a97edd7d3956bac17
Brobbey <wirekobrobbey27@gmail.com>
2023-08-19 14:45:23 -0500

    Second approach

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-const {readFile} = require('fs')
+const {readFile, writeFile} = require('fs')
 
-// readFile(path,()=>{})
-const getText = (path)=>{
-    return new Promise((resolve, reject)=>{
-        readFile(path,'utf8',(err, data)=> {
-            if(err){
-                reject(err)
-            }else{
-                resolve(data)
-            }
-        })
-    })
+const util = require('util')
+
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+
+const start = async ()=>{
+    try{
+        const first = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        await writeFilePromise('./content/dropping-some-grenade.txt',`Awesome smilga: ${first} : ${second}`)
+        console.log(first, second)
+    }catch(e){
+        console.log(e)
+    }
 }
+// readFile(path,()=>{})
+// const getText = (path)=>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path,'utf8',(err, data)=> {
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
 // getText('./content/first.txt').then((data)=>{
 //     console.log(data)
 // }).catch((err)=>{
@@ -20,12 +36,12 @@ const getText = (path)=>{
 
 
 // Using async block
-const start = async()=>{
-    try{
-        const first = await getText('./content/first.txt');
-        console.log(first)
-    }catch(e){
-        console.log(e)
-    }
-}
+// const start = async()=>{
+//     try{
+//         const first = await getText('./content/first.txt');
+//         console.log(first)
+//     }catch(e){
+//         console.log(e)
+//     }
+// }
 start()
